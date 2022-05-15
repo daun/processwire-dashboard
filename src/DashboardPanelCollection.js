dbda4c20332bfcfd2c47ea96c902fa50cd5ff492
Philipp Daun <post@philippdaun.net>
2022-05-15 21:19:11 +0200

    Render pagination links in collection panel

--- src/DashboardPanelCollection.js
+++ src/DashboardPanelCollection.js
@@ -3,8 +3,22 @@
 function initPanel($panel) {
   const $reloadlinks = $panel.find('.pw-modal[data-reload-on-close]');
   if ($reloadlinks.length) {
+    const page = $panel.data('page') || 1;
+    const params = { page };
     $reloadlinks.on('pw-modal-closed', () => {
-      $panel.trigger('reload', { animate: true });
+      $panel.trigger('reload', { animate: false, params });
+    });
+  }
+
+  const $paginationLinks = $panel.find('a[data-pagination]');
+  if ($paginationLinks.length) {
+    $paginationLinks.on('click', (event) => {
+      event.preventDefault();
+      const url = event.currentTarget.href;
+      const page = (url.match(/\d+$/) || [])[0] || 1;
+      const params = { page };
+      $panel.data('page', page);
+      $panel.trigger('reload', { animate: false, params });
     });
   }
 
